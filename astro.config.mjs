67e0efa0dd018c99190f59a41860ad49246c88d6
kangyeop.lee <lky5697@naver.com>
2025-04-05 20:38:37 +0900

    feat: init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite:{
+        optimizeDeps: {
+            include: ['swiper', 'swiper/modules']
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        swiper: ['swiper', 'swiper/modules']
+                    }
+                }
+            }
+        }
+    }
+});
